82a695b1ab565b917ed4073f767c833908bc599c
ben <benjaminalexanderfox11@gmail.com>
2022-10-26 21:52:54 -0400

    removed logger

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import logger from '../utils/logger'
+import log from '../utils/logger'
 import { createUser } from '../service/user.service'
 import { CreateUserInput } from '../schema/user.schema'
 import { omit } from 'lodash'
@@ -7,7 +7,6 @@ import { omit } from 'lodash'
 export async function createUserHandler(req: Request<{}, {}, CreateUserInput['body']>, res: Response) {
   try {
     const user = await createUser(req.body)
-
     return res.status(201).send(omit(user, 'password'))
   } catch (error: any) {
     return res.status(400).send({
@@ -26,7 +25,7 @@ export async function getCurrentUser(req: Request, res: Response) {
     const user = res.locals.user
     return res.send(user)
   } catch (error: any) {
-    logger.error(error)
+    log.error(error)
     return res.status(500).send({
       errors: [
         {
