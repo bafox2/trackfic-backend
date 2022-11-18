1f186abfc7dc4f75e1a9b51ab48a17758da903b6
ben <benjaminalexanderfox11@gmail.com>
2022-11-18 15:10:38 -0500

    missing dependencies dotenv

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import config from 'config'
 import connect from './utils/connect'
 import log from './utils/logger'
 import createServer from './utils/server'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const port = config.get<number>('port')
 const app = createServer()
