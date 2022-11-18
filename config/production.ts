45e57fa6d7c63b713b4e07dae9e4250820cc4139
ben <benjaminalexanderfox11@gmail.com>
2022-11-18 18:10:25 -0500

    need custom env recognized4

--- config/production.ts
+++ config/production.ts
@@ -1,3 +1,17 @@
 export default {
-  
+  privateKeyEncoded: "ACCESS_TOKEN_PRIVATE_KEY",
+  publicKeyEncoded: "ACCESS_TOKEN_PUBLIC_KEY",
+  dbUri: "DB_CONNECTION",
+  port: "PORT",
+  domain: 'DOMAIN',
+  googleAPIKey: 'GOOGLE_API_KEY',
+  saltWorkFactor: 'SALT_WORK_FACTOR',
+  accessTokenTtl: 'ACCESS_TOKEN_TTL',
+  refreshTokenTtl: 'REFRESH_TOKEN_TTL',
+  accessTokenTtlMs: 'ACCESS_TOKEN_TTL_MS',
+  refreshTokenTtlMs: 'REFRESH_TOKEN_TTL_MS',
+  timezone: 'America/New_York',
+  mapsAPI: 'MAPS_API',
+  nodeConfigDir: 'NODE_CONFIG_DIR',
+  clientUrl: 'CLIENT_URL',
 }
