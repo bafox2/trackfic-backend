fe6c7d98ce7f5400348dfc5a8ea74d406e384193
ben <benjaminalexanderfox11@gmail.com>
2022-11-20 12:37:06 -0500

    wrong domain troubleshooting extra / 8

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -25,7 +25,8 @@ export async function createUserSessionHandler(req: Request, res: Response) {
   res.cookie('accessToken', accessToken, {
     maxAge: config.get('accessTokenTtlMs'),
     httpOnly: true,
-    domain: `${config.get('clientUrl')}/`,
+    // @ts-ignore
+    domain: `${config.get('clientUrl').slice(8)}`,
     path: 'api/sessions',
     sameSite: 'strict',
     secure: false,
@@ -34,7 +35,8 @@ export async function createUserSessionHandler(req: Request, res: Response) {
   res.cookie('refreshToken', refreshToken, {
     maxAge: config.get('refreshTokenTtlMs'),
     httpOnly: true,
-    domain: `${config.get('clientUrl')}/`,
+    // @ts-ignore
+    domain: `${config.get('clientUrl').slice(8)}`,
     path: 'api/sessions',
     sameSite: 'strict',
     secure: false,
