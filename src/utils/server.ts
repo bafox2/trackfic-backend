c6ed15a5c49e38c3b26d12e14162a6054879a416
ben <ben@bfox.dev>
2022-12-01 21:09:07 -0500

    added localhost to cors for iteration

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -9,7 +9,7 @@ function createServer() {
   const app = express()
   app.use(
     cors({
-      origin: config.get('clientUrl'),
+      origin: [config.get('clientUrl'), 'http://localhost:3000'],
       credentials: true,
     })
   )
